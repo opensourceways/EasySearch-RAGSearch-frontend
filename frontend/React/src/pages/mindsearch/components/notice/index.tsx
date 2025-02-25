137a76398cd8d24bfb257764e8eb55c8cb5bcf34
ZWJason <zwjsec@mail.ustc.edu.cn>
2025-02-25 20:26:53 +0800

    init commit

---
+++ frontend/React/src/pages/mindsearch/components/notice/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.less';
+import IconFont from '../iconfont';
+
+const Notice = () => {
+    return <>
+        <div className={styles.notice}>openEuler 是由开放原子开源基金会（OpenAtom Foundation）孵化及运营的开源项目</div>
+    </>;
+};
+export default Notice;
